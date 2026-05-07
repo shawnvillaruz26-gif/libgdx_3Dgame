466627e5ce0573257be0cf3e331293c57ee8a24b
shawnvillaruz26-gif <shawnvillaruz26@gmail.com>
2026-05-08 04:22:54 +0800

    Update DisasterRunGame.java

--- DisasterRunGame.java
+++ DisasterRunGame.java
@@ -1,49 +1,72 @@
-package com.brv.disasterrun3d;
+package com.brv.disasterrun;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.kotcrab.vis.ui.VisUI;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController;
 
-/**
- * Main Game Class para sa Disaster Run.
- * Ito ang taga-manage kung anong screen ang dapat ipakita (Menu o 3D Game).
- */
-public class DisasterRunGame extends Game {
+public class DisasterRunGame extends ApplicationAdapter {
+    private PerspectiveCamera cam;
+    private ModelBatch modelBatch;
+    private Model model;
+    private ModelInstance instance;
+    private Environment environment;
+    private FirstPersonCameraController camController;
 
     @Override
     public void create() {
-        // 1. I-load ang VisUI Skin.
-        // Mahalaga ito para gumana ang mga buttons at UI elements sa MenuScreen.
-        try {
-            if (!VisUI.isLoaded()) {
-                VisUI.load();
-            }
-        } catch (Exception e) {
-            Gdx.app.error("DisasterRunGame", "Hindi ma-load ang VisUI: " + e.getMessage());
-        }
+        modelBatch = new ModelBatch();
 
-        // 2. Itakda ang unang screen na lilitaw (Menu Screen).
-        // Ipinapasa natin ang 'this' para makapag-switch ng screen ang Menu mamaya.
-        this.setScreen(new MenuScreen(this));
+        // 1. Setup Camera (Posisyon sa 3D space)
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(10f, 10f, 10f);
+        cam.lookAt(0,0,0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
+
+        // 2. Mouse & Keyboard Control (WASD para gumalaw, Mouse para tumingin)
+        camController = new FirstPersonCameraController(cam);
+        Gdx.input.setInputProcessor(camController);
+
+        // 3. Lighting (Para hindi mukhang flat ang 3D)
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+        // 4. Gumawa ng Test Box (Habang wala pa tayong Blender model)
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createBox(5f, 5f, 5f,
+            new Material(ColorAttribute.createDiffuse(Color.FIREBRICK)),
+            Usage.Position | Usage.Normal);
+        instance = new ModelInstance(model);
     }
 
     @Override
     public void render() {
-        // Napaka-importante: Tawagin ang super.render()
-        // para mag-update at mag-draw ang kasalukuyang screen.
-        super.render();
+        // Update movement base sa pinipindot mo
+        camController.update();
+
+        // Linisin ang screen
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        // I-draw ang 3D Box
+        modelBatch.begin(cam);
+        modelBatch.render(instance, environment);
+        modelBatch.end();
     }
 
     @Override
     public void dispose() {
-        // 1. Linisin ang kasalukuyang screen para iwas memory leak.
-        if (getScreen() != null) {
-            getScreen().dispose();
-        }
-
-        // 2. Linisin ang VisUI resources pag-exit ng laro.
-        if (VisUI.isLoaded()) {
-            VisUI.dispose();
-        }
+        modelBatch.dispose();
+        model.dispose();
     }
 }
